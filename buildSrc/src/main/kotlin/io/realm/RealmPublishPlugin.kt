8f159d3deed92a404f8d8e0983fb2011e1326d13
Denis Popkov <popkov-den03@mail.ru>
2025-06-30 10:28:52 +0600

    fix: update some modules

--- buildSrc/src/main/kotlin/io/realm/RealmPublishPlugin.kt
+++ buildSrc/src/main/kotlin/io/realm/RealmPublishPlugin.kt
@@ -36,13 +36,11 @@ import java.io.File
 import java.time.Duration
 import java.net.URI
 
-// Custom options for POM configurations that might differ between Realm modules
 open class PomOptions {
     open var name: String = ""
     open var description: String = ""
 }
 
-// Configure how the Realm module is published
 open class RealmPublishExtensions {
     open var pom: PomOptions = PomOptions()
     open fun pom(action: Action<PomOptions>) {
@@ -64,10 +62,8 @@ fun hasProperty(project: Project, propertyName: String): Boolean {
     return projectProp || (systemProp != null && systemProp.isNotEmpty())
 }
 
-// Plugin responsible for handling publishing to mavenLocal and Maven Central.
 class RealmPublishPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
-        // Configure constants required by the publishing process
         val signBuild: Boolean = hasProperty(project,"signBuild")
         configureSignedBuild(signBuild, this)
     }
@@ -108,10 +104,6 @@ class RealmPublishPlugin : Plugin<Project> {
     }
 
     private fun configureSignedBuild(signBuild: Boolean, project: Project) {
-        // The nexus publisher plugin can only be applied to top-level projects.
-        // See https://github.com/gradle-nexus/publish-plugin/issues/81
-        // Also, we should not apply the MavenPublish plugin to the root project as it will result in an
-        // empty `realm-kotlin` artifact being deployed to Maven Central.
         val isRootProject: Boolean = (project == project.rootProject)
         if (isRootProject) {
             configureRootProject(project)
@@ -123,21 +115,13 @@ class RealmPublishPlugin : Plugin<Project> {
     }
 
     private fun configureSubProject(project: Project, signBuild: Boolean) {
-        // ID for the Realm Kotlin PGP key file.
         val keyId = "1F48C9B0"
-        // Apparently Gradle treats properties define through a gradle.properties file differently
-        // than those defined through the commandline using `-P`. This is a problem with new
-        // line characters as found in an ascii-armoured PGP file. To ensure we can work around this,
-        // all newlines have been replaced with `#` and thus needs to be reverted here.
         val ringFile: String = getPropertyValue(project,"signSecretRingFileKotlin").replace('#', '\n')
         val password: String = getPropertyValue(project, "signPasswordKotlin")
 
         with(project) {
             plugins.apply(SigningPlugin::class.java)
             plugins.apply(MavenPublishPlugin::class.java)
-
-            // Create the RealmPublish plugin. It must evaluate after all other plugins as it modifies their output.
-            // Only allow configuration from sub projects as the top-level project is just a placeholder
             extensions.create<RealmPublishExtensions>("realmPublish")
 
             afterEvaluate {
@@ -146,7 +130,6 @@ class RealmPublishPlugin : Plugin<Project> {
                 }
             }
 
-            // Configure signing of artifacts
             extensions.getByType<SigningExtension>().apply {
                 isRequired = signBuild
                 useInMemoryPgpKeys(keyId, ringFile, password)
@@ -156,29 +139,7 @@ class RealmPublishPlugin : Plugin<Project> {
     }
 
     private fun configureRootProject(project: Project) {
-        val sonatypeStagingProfileId = "78c19333e4450f"
 
-        with(project) {
-            project.plugins.apply(NexusPublishPlugin::class.java)
-
-            // Configure upload to Maven Central.
-            // The nexus publisher plugin can only be applied to top-level projects.
-            // See https://github.com/gradle-nexus/publish-plugin/issues/81
-            extensions.getByType<NexusPublishExtension>().apply {
-                this.packageGroup.set("io.realm.kotlin")
-                this.repositories {
-                    sonatype {
-                        this.stagingProfileId.set(sonatypeStagingProfileId)
-                        this.username.set(getPropertyValue(project,"ossrhUsername"))
-                        this.password.set(getPropertyValue(project,"ossrhPassword"))
-                    }
-                }
-                this.transitionCheckOptions {
-                    maxRetries.set(720) // Retry for 2 hours. Sometimes Maven Central is really slow!
-                    delayBetween.set(Duration.ofSeconds(10))
-                }
-            }
-        }
     }
 
     private fun configurePom(project: Project, options: PomOptions) {
